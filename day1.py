5c934049a1a2e13e41cc95eccf59364ae7ffe346
Yoyo Lam <mtcyoyo@gmail.com>
2019-12-04 17:01:05 +0100

    Updated day1 to print answers to both parts

--- day1.py
+++ day1.py
@@ -1,15 +1,17 @@
 import math
 
 
-def fuel_requirement(mass):
+def fuel_requirement(mass, part1=False):
     fr = math.floor(mass / 3) - 2
+    if part1:
+        return fr
     if fr <= 0:
         return 0
     return fr + fuel_requirement(fr)
 
+
 with open("day1-input.txt", "r") as f:
     fuel_requirements = 0
-    for mass in f:
-        fuel_requirements += fuel_requirement(int(mass))
-
-print(fuel_requirements)
+    masses = f.readlines()
+    print(sum([fuel_requirement(int(mass), part1=True) for mass in masses]))
+    print(sum([fuel_requirement(int(mass)) for mass in masses]))
