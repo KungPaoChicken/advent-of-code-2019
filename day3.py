78a2013d012e7fc58fcf0714d4fb5ccef2ece572
Yoyo Lam <mtcyoyo@gmail.com>
2019-12-06 21:20:03 +0100

    Added part 2 answers to day 3 and day 4

--- day3.py
+++ day3.py
@@ -30,33 +30,16 @@ def find_intersections(lines1, lines2):
     intersections = []
     for (x0, y0), (x1, y1) in sorted(lines1):
         for (i0, j0), (i1, j1) in sorted(lines2):
-            if is_vertical(x0, x1) and is_vertical(i0, i1):
-                if x0 == i0:
-                    intersections.extend(
-                        [
-                            (x0, y)
-                            for y in linear_intersections((y0, y1 + 1), (j0, j1 + 1))
-                        ]
-                    )
-            elif not is_vertical(x0, x1) and not is_vertical(i0, i1):
-                if y0 == j0:
-                    intersections.extend(
-                        [
-                            (x, y0)
-                            for x in linear_intersections((x0, x1 + 1), (i0, i1 + 1))
-                        ]
-                    )
+            if is_vertical(x0, x1):
+                if (min(y0, y1) <= j0 <= max(y0, y1)) and (
+                    min(i0, i1) <= x0 <= max(i0, i1)
+                ):
+                    intersections.append((x0, j0))
             else:
-                if is_vertical(x0, x1):
-                    if min(y0, y1) <= j0 <= max(y0, y1) and min(i0, i1) <= x0 <= max(
-                        i0, i1
-                    ):
-                        intersections.append((x0, j0))
-                else:
-                    if min(j0, j1) <= y0 <= max(j0, j1) and min(x0, x1) <= i0 <= max(
-                        x0, x1
-                    ):
-                        intersections.append((i0, y0))
+                if (min(j0, j1) <= y0 <= max(j0, j1)) and (
+                    min(x0, x1) <= i0 <= max(x0, x1)
+                ):
+                    intersections.append((i0, y0))
     return intersections
 
 
@@ -64,13 +47,26 @@ def manhattan_distance(x, y):
     return abs(x) + abs(y)
 
 
+def wire_distance(lines, x, y):
+    distance = 0
+    for (x0, y0), (x1, y1) in lines:
+        if min(x0, x1) <= x <= max(x0, x1) and min(y0, y1) <= y <= max(y0, y1):
+            return distance + abs(y - y0) + abs(x - x0)
+        else:
+            distance += abs(y1 - y0) + abs(x1 - x0)
+
+
 with open("day3-input.txt", "r") as f:
     wire1 = f.readline()
     wire2 = f.readline()
-    lines1 = directions_to_coordinates(wire1.split(","))
-    lines2 = directions_to_coordinates(wire2.split(","))
-    intersections = find_intersections(lines1, lines2)
-    distances = [manhattan_distance(*i) for i in intersections]
-    if 0 in distances:
-        distances.remove(0)
-    print(min(distances))
+
+lines1 = directions_to_coordinates(wire1.split(","))
+lines2 = directions_to_coordinates(wire2.split(","))
+intersections = find_intersections(lines1, lines2)
+intersections.remove((0, 0))
+distances = [manhattan_distance(*i) for i in intersections]
+print(min(distances))
+wire_distances = [
+    wire_distance(lines1, *i) + wire_distance(lines2, *i) for i in intersections
+]
+print(min(wire_distances))
--- day4.py
+++ day4.py
@@ -1,6 +1,3 @@
-inputs = (356261, 846303 + 1)
-
-
 def is_increasing(number):
     s = str(number)
     for i in range(len(s) - 1):
@@ -9,16 +6,26 @@ def is_increasing(number):
     return True
 
 
+from collections import Counter
+
+
 def has_double(number):
-    s = str(number)
-    for i in range(len(s) - 1):
-        if s[i + 1] == s[i]:
-            return True
-    return False
+    c = Counter(str(number))
+    return max(c.values()) > 1
 
 
-def is_password(number):
+def has_at_least_one_double(number):
+    c = Counter(str(number))
+    return 2 in c.values()
+
+
+def is_part1_password(number):
     return is_increasing(number) and has_double(number)
 
 
-print(len(list(filter(is_password, range(*inputs)))))
+inputs = (356261, 846303 + 1)
+
+part1_passwords = list(filter(is_part1_password, range(*inputs)))
+print(len(part1_passwords))
+print(len(list(filter(has_at_least_one_double, part1_passwords))))
+
