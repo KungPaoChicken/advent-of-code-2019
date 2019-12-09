8ddb3cfa9f9ed6e67dfaba3d0e29d0d481621046
KungPaoChicken <mtcyoyo@gmail.com>
2019-12-09 03:39:49 +0100

    Plug program input into parser directly

--- day5.py
+++ day5.py
@@ -1,11 +1,6 @@
 from intcode_computer import str_to_program, parse_intcode
 
-inputs = str_to_program(open("day5-input.txt").read())
+program = str_to_program(open("day5-input.txt").read())
 
-# Input 1
-print("Part 1: Input 1")
-parse_intcode(inputs.copy(), 0, disable_jumps=True)
-
-# Input 5
-print("Part 2: Input 5")
-parse_intcode(inputs.copy(), 0)
+parse_intcode(program.copy(), 0, [1], disable_jumps=True)
+parse_intcode(program.copy(), 0, [5])
