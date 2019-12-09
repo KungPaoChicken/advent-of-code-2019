fcd406519c83db2fe71c9c07c2a1dc4c262fca30
KungPaoChicken <mtcyoyo@gmail.com>
2019-12-09 04:41:58 +0100

    Add output parameter to parse_intcode()

--- day5.py
+++ day5.py
@@ -2,7 +2,7 @@ from intcode_computer import str_to_program, parse_intcode
 
 program = str_to_program(open("day5-input.txt").read())
 
-_, part1 = parse_intcode(program.copy(), 0, [1], disable_jumps=True)
+_, part1 = parse_intcode(program.copy(), 0, [1], [], disable_jumps=True)
 print(part1)
-_, part2 = parse_intcode(program.copy(), 0, [5])
+_, part2 = parse_intcode(program.copy(), 0, [5], [])
 print(part2)
