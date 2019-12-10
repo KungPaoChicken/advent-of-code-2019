a08a728300156c7a202991670a8603c6179bfe95
KungPaoChicken <mtcyoyo@gmail.com>
2019-12-10 03:42:24 +0100

    Added day 7 part 2

--- day7.py
+++ day7.py
@@ -17,16 +17,15 @@ def amplifiers_config2(amplifiers, phase_settings, input_signal=0):
     inputs = [[p] for p in phase_settings]
     inputs[0].append(input_signal)
     while True:
-        print("IN ", inputs[i])
-        print("MEM ", amplifiers[i][26:29])
         amplifiers[i], ip[i], inputs[i], output = parse_intcode(
             amplifiers[i], ip[i], inputs[i], [], quit_on_output=True
         )
-        if not output:
-            return input_signal
-        print("OUT", output[0])
-        inputs[(i + 1) % 5].append(output[0])
-        i = (i + 1) % 5
+        if output:
+            last_output = output[0]
+            inputs[(i + 1) % 5].append(output[0])
+            i = (i + 1) % 5
+            continue
+        return last_output
 
 
 def create_setup(amplifiers, config):
@@ -43,17 +42,8 @@ def max_thruster_signal(amplifier_setup, phase_permutations):
 
 program = str_to_program(open("day7-input.txt", "r").read())
 
-setup1 = create_setup([program] * 5, amplifiers_config1)
+setup1 = create_setup([program.copy() for i in range(5)], amplifiers_config1)
 print(max_thruster_signal(setup1, [0, 1, 2, 3, 4]))
 
-program = str_to_program(
-    "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5"
-)
-print(amplifiers_config2([program] * 5, [9, 8, 7, 6, 5]))
-# program = str_to_program(
-#     "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10"
-# )
-# print(amplifiers_config2([program] * 5, [9, 7, 8, 5, 6]))
-
-setup2 = create_setup([program] * 5, amplifiers_config2)
-# print(max_thruster_signal(setup2, [5, 6, 7, 8, 9]))
+setup2 = create_setup([program.copy() for i in range(5)], amplifiers_config2)
+print(max_thruster_signal(setup2, [5, 6, 7, 8, 9]))
